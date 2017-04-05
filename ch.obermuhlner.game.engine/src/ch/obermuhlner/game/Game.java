e59bf7bb30011d15d034d2ac20f513d6c0a2bce5
Eric Obermühlner <eobermuhlner@gmail.com>
2017-04-05 07:41:35 +0200

    default method getValidMoves()

--- ch.obermuhlner.game.engine/src/ch/obermuhlner/game/Game.java
+++ ch.obermuhlner.game.engine/src/ch/obermuhlner/game/Game.java
@@ -1,6 +1,7 @@
 package ch.obermuhlner.game;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public interface Game<G extends Game<G>> {
 
@@ -17,6 +18,12 @@ public interface Game<G extends Game<G>> {
 	Map<String, Double> getAllMoves();
 	
 	boolean isValid(String move);
+	
+	default Map<String, Double> getValidMoves() {
+		return getAllMoves().entrySet().stream()
+				.filter(entry -> isValid(entry.getKey()))
+				.collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()));
+	}
 	
 	boolean isFinished();
 	
