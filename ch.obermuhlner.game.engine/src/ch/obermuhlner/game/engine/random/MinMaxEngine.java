ba8800acf1ff9beda0384bbeef113274c7ffb8f3
Eric Obermühlner <eobermuhlner@gmail.com>
2017-04-09 18:30:52 +0200

    add alpha/beta pruning to min/max tree search

--- ch.obermuhlner.game.engine/src/ch/obermuhlner/game/engine/random/MinMaxEngine.java
+++ ch.obermuhlner.game.engine/src/ch/obermuhlner/game/engine/random/MinMaxEngine.java
@@ -3,7 +3,6 @@ package ch.obermuhlner.game.engine.random;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
 
 import ch.obermuhlner.game.Engine;
 import ch.obermuhlner.game.Game;
@@ -55,10 +54,10 @@ public class MinMaxEngine<G extends Game> implements Engine<G> {
 		Game local = game.cloneGame();
 		local.move(move);
 		
-		return Tuple2.of(move, minimax(local, targetDepth, false));
+		return Tuple2.of(move, minimax(local, targetDepth, LOSS_VALUE, WIN_VALUE, false));
 	}
 	
-	private static <G extends Game> double minimax(G game, int depth, boolean maximizePlayer) {
+	private static <G extends Game> double minimax(G game, int depth, double alpha, double beta, boolean maximizePlayer) {
 		Side sideToMove = game.getSideToMove();
 
 		if (game.isFinished()) {
@@ -75,20 +74,26 @@ public class MinMaxEngine<G extends Game> implements Engine<G> {
 		if (depth == 0) {
 			return game.getScore();
 		}
-		
-		DoubleStream valueStream = game.getValidMoves().stream()
-				.map(moveWithValue -> moveWithValue.getValue1())
-				.mapToDouble(move -> {
-					Game local = game.cloneGame();
-					local.move(move);
-					return minimax(local, depth - 1, !maximizePlayer);
-				});
-		
-		double bestValue;
-		if (maximizePlayer) {
-			bestValue = valueStream.max().getAsDouble();
-		} else {
-			bestValue = valueStream.min().getAsDouble();			
+
+		double bestValue = maximizePlayer ? LOSS_VALUE : WIN_VALUE;
+
+		List<Tuple2<String, Double>> validMoves = game.getValidMoves();
+		for (Tuple2<String, Double> moveWithValue : validMoves) {
+			String move = moveWithValue.getValue1();
+
+			Game local = game.cloneGame();
+			local.move(move);
+			double value = minimax(local, depth - 1, alpha, beta, !maximizePlayer);
+			if (maximizePlayer) {
+				bestValue = Math.max(bestValue, value);
+				alpha = Math.max(alpha, value);
+			} else {
+				bestValue = Math.min(bestValue, value);
+				beta = Math.min(beta, value);
+			}
+			if (beta <= alpha) {
+				break;
+			}
 		}
 		
 		return bestValue;
