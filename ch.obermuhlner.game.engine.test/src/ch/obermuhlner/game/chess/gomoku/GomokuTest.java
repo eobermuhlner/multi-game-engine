887140aff4709b222940c177c11c1d79de718b77
Eric Obermühlner <eobermuhlner@gmail.com>
2017-04-15 20:38:04 +0200

    add Gomoku test

---
+++ ch.obermuhlner.game.engine.test/src/ch/obermuhlner/game/chess/gomoku/GomokuTest.java
@@ -0,0 +1,27 @@
+package ch.obermuhlner.game.chess.gomoku;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ch.obermuhlner.game.gomoku.Gomoku;
+
+public class GomokuTest {
+
+	@Test
+	public void testGetState() {
+		Gomoku gomoku = new Gomoku();
+		gomoku.setStartPosition();
+		assertEquals("19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19 b", gomoku.getState());
+		
+		gomoku.move("aa");
+		assertEquals("19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/b18 w", gomoku.getState());
+	}
+	
+	@Test
+	public void testSetState() {
+		Gomoku gomoku = new Gomoku();
+		gomoku.setState("19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/b18 w");
+		assertEquals("19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/19/b18 w", gomoku.getState());
+	}
+}
