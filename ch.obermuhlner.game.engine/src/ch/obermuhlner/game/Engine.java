af70c8db102e0bc654a8a9015c37dddbbc66ce6c
Eric Obermühlner <eobermuhlner@gmail.com>
2017-04-18 07:38:10 +0200

    javadoc for Engine

--- ch.obermuhlner.game.engine/src/ch/obermuhlner/game/Engine.java
+++ ch.obermuhlner.game.engine/src/ch/obermuhlner/game/Engine.java
@@ -1,11 +1,32 @@
 package ch.obermuhlner.game;
 
+/**
+ * Calculates the best move for a given game situation.
+ *
+ * @param <G> the game
+ */
 public interface Engine<G extends Game> {
 
+	/**
+	 * Returns the current game.
+	 * 
+	 * @return the game
+	 */
 	G getGame();
-	
+
+	/**
+	 * Returns the best move after calculating for a default time or to a default depth.
+	 * 
+	 * @return the best move
+	 */
 	String bestMove();
 
+	/**
+	 * Returns a {@link StoppableCalculation} that calculates the best move.
+	 * 
+	 * @param milliseconds the given time to calculate the best move 
+	 * @return the {@link StoppableCalculation} that will return the best move
+	 */
 	StoppableCalculation<String> bestMove(long milliseconds);
 
 }
