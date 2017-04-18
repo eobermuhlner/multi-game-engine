458d0266cee54d589084d2d93221a101d68d183b
Eric Obermühlner <eobermuhlner@gmail.com>
2017-04-18 07:44:01 +0200

    javadoc for Side

--- ch.obermuhlner.game.engine/src/ch/obermuhlner/game/Side.java
+++ ch.obermuhlner.game.engine/src/ch/obermuhlner/game/Side.java
@@ -1,10 +1,20 @@
 package ch.obermuhlner.game;
 
+/**
+ * The side of a game: white, black or none.
+ */
 public enum Side {
 	None,
 	White,
 	Black;
 	
+	/**
+	 * Returns the other side of this side.
+	 * 
+	 * @return {@link #White} returns {@link #Black},<br>
+	 *         {@link #Black} returns {@link #White},<br>
+	 *         {@link #None} returns {@link #None},<br>
+	 */
 	public Side otherSide() {
 		return otherSide(this);
 	}
